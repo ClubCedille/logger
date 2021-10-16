b165f51c6e97f65c5ad16904aa97e44dbd89045c
Charles Guertin <charlesguertin@live.ca>
2021-10-15 22:52:25 -0400

    Added source code for logger.

---
+++ logger.go
@@ -0,0 +1,103 @@
+package logger
+
+import (
+	"context"
+	"io"
+	"os"
+	"sync"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	LoggerCtxKey = "logger"
+)
+
+var (
+	defaultLogger    log.FieldLogger
+	defaultLevel     = log.InfoLevel
+	defaultOutput    = os.Stdout
+	defaultFormatter = &log.TextFormatter{
+		TimestampFormat: time.RFC3339,
+		FullTimestamp:   true,
+	}
+
+	syncOnce = sync.Once{}
+)
+
+type LoggerConfig struct {
+	Level     string
+	Output    io.Writer
+	Formatter log.Formatter
+}
+
+// Initialize -
+// Initializes a new Logger from a given configuration.
+// If config is empty, default settings are used.
+func Initialize(config LoggerConfig) log.FieldLogger {
+	syncOnce.Do(func() {
+		newLogger := &log.Logger{}
+		newLogger.SetNoLock()
+
+		// Set Formatter
+		if config.Formatter == nil {
+			newLogger.SetFormatter(defaultFormatter)
+		} else {
+			newLogger.SetFormatter(config.Formatter)
+		}
+
+		// Set Output
+		if config.Output == nil {
+			newLogger.SetOutput(defaultOutput)
+		} else {
+			newLogger.SetOutput(config.Output)
+		}
+
+		// Set Level
+		if lvl, err := log.ParseLevel(config.Level); err != nil {
+			newLogger.SetLevel(defaultLevel)
+		} else {
+			newLogger.SetLevel(lvl)
+		}
+
+		// Only add method as field if debugging.
+		if newLogger.GetLevel() == log.DebugLevel {
+			// Add calling method as a field
+			newLogger.SetReportCaller(true)
+		}
+
+		// Assign struct to outgoing interface
+		defaultLogger = newLogger
+	})
+	return defaultLogger
+}
+
+// NewFromContextOrDefault -
+// Returns a Logger from a given Context instance.
+// If the Context doesn't contain the Logger,
+// a default Logger is returned.
+func NewFromContextOrDefault(ctx context.Context) log.FieldLogger {
+	if ctx == nil {
+		return initDefaultLogger()
+	}
+
+	// Fetch logger from context
+	ctxVal := ctx.Value(LoggerCtxKey)
+	if ctxVal == nil {
+		return initDefaultLogger()
+	}
+
+	// Cast logger to FieldLogger interface
+	if logger, ok := ctxVal.(log.FieldLogger); ok {
+		return logger
+	}
+
+	return initDefaultLogger()
+}
+
+func initDefaultLogger() log.FieldLogger {
+	Initialize(LoggerConfig{})
+	defaultLogger.Warn("logger not found in context, using default logger")
+	return defaultLogger
+}
